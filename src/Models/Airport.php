20d9d7f716793852a74b1357fb6495ac433b962a
Jan <jan.thoelken@gmail.com>
2021-10-02 21:13:36 +0200

    Update Airport.php

--- src/Models/Airport.php
+++ src/Models/Airport.php
@@ -13,7 +13,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Airport extends Model
 {
-    protected $table = 'airports';
+    protected $table = 'airports_global';
     
     protected $fillable = ['id', 'ident', 'type', 'name', 'latitude_deg', 'longitude_deg', 'elevation_ft', 'continent', 'iso_country', 'iso_region', 'municipality', 'scheduled_service', 'gps_code', 'iata_code', 'local_code', 'home_link', 'wikipedia_link', 'keywords'];
 }
